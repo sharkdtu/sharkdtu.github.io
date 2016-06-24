b8b447e285242904196394fd575f0ed863f76854
sharkd <sharkd.tu@gmail.com>
2016-06-25 01:37:09 +0800

    revert forward statement

--- themes/next/scripts/copyright.js
+++ themes/next/scripts/copyright.js
@@ -7,7 +7,7 @@ hexo.extend.filter.register('before_post_render', function(data){
     if(data.copyright == false) return data;
     if(data.content.length > 50)
     {
-        data.content += '\n<span style="color:red"><em>本文来自<a style="color:red" href="http://sharkdtu.com/">守护之鲨</a>，转载请注明出处：' + data.permalink + '</em></span>';
+        data.content += '\n<span style="color:red"><em>转载请注明出处，本文永久链接：' + data.permalink + '</em></span>';
     }
     return data;
 });
